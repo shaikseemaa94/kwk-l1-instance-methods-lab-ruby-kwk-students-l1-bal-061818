850f03781d4b41f2acfc5e496035b5015ec14651
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-22 19:47:09 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 # person.rb
 class Person
   def talk
-    puts "Hello World"
+    puts "Hello World !"
   end
   def walk
-    puts "The person is walking."
+    puts "The Person is walking"
   end
 end
